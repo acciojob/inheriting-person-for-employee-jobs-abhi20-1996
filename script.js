a9ecd61253a01c258aa631ffacd76a06bd3f927c
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-04-27 21:50:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // complete this js code
 function Person(name, age) {
-   void	greet(){
+   this.greet(){
 		consol.log("Hello, my name is"+ name +", I am" + age +" years old")
 	}
 }
 
 function Employee extends Person(name, age, jobTitle) {
-	void jobGreet(){
+	this.jobGreet(){
 		console.log("Hello, my name is "+name+", I am "+age+
 					" years old, and my job title is "+jobTitle)
 	}
