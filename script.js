902d3e1e0aeba834f4aa622b971f60b62802487f
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-06-03 11:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,11 @@ class Employee extends Person {
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
     }
 }
-
+const temp_person = new Person("abc", 10); 
+temp_person.greet(); 
+const temp_employee = new Employee("def", 20, "Developer"); 
+temp_employee.greet(); temp_employee.jobGreet();
 // Do not change code below this line
+
 window.Person = Person;
 window.Employee = Employee;
